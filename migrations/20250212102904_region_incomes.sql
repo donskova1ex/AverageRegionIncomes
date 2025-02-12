ecdddcc3b9c99046f754ca47003c3f98a9cce9e8
Alex Donskov <donskov_a@icloud.com>
2025-02-12 19:17:19 +0500

    TODO

--- migrations/20250212102904_region_incomes.sql
+++ migrations/20250212102904_region_incomes.sql
@@ -4,10 +4,11 @@ CREATE TABLE IF NOT EXISTS region_incomes (
     RegionId INTEGER PRIMARY KEY,
     Year INTEGER,
     Quarter INTEGER,
-    Value FLOAT);
+    Value DECIMAL(18,2));
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
 DROP TABLE IF EXISTS region_incomes;
 -- +goose StatementEnd
+//TODO: Формат данных не отрицательный
