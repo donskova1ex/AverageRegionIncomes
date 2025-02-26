4dde671ac74b842aa9356b41708806eb0ebc27ed
Donskov <donskov_a@icloud.com>
2025-02-26 16:29:55 +0500

    adding constraint

--- migrations/20250212102904_region_incomes.sql
+++ migrations/20250212102904_region_incomes.sql
@@ -1,10 +1,13 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS region_incomes (
-    RegionId INTEGER PRIMARY KEY,
-    Year INTEGER,
-    Quarter INTEGER,
-    Value DECIMAL(18,2));
+    RegionId INTEGER NOT NULL,
+    Year INTEGER NOT NULL,
+    Quarter INTEGER NOT NULL,
+    Value DECIMAL(18,2) NOT NULL,
+    CONSTRAINT PK_RegionIncomes PRIMARY KEY (RegionId, Year, Quarter, Value),
+    CONSTRAINT CHK_ValueNonNegative CHECK (Value >= 0)
+    );
 -- +goose StatementEnd
 
 -- +goose Down
