4bc7e969960172cc4eeff6e17bdb5b2b82efcd76
Donskov <donskov_a@icloud.com>
2025-03-10 15:50:09 +0500

    adding request duration

--- internal/middleware/logger.go
+++ internal/middleware/logger.go
@@ -3,21 +3,38 @@ package middleware
 import (
 	"log/slog"
 	"net/http"
+	"strings"
+	"time"
 )
 
 func RequestLogger(logger *slog.Logger) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+
 			rw := NewCommonResponseWriter(w)
 			next.ServeHTTP(rw, r)
-			requestID, _ := r.Context().Value(RequestIDCtxKey).(RequestID)
-			logger.Info(
-				"http request",
+
+			log := logger.With(
 				slog.String("method", r.Method),
 				slog.String("path", r.URL.Path),
+			)
+
+			queryParams := r.URL.Query()
+			if len(queryParams) > 0 {
+				for key, values := range queryParams {
+					log = log.With(slog.String(key, strings.Join(values, ",")))
+				}
+			}
+
+			requestID, _ := r.Context().Value(RequestIDCtxKey).(RequestID)
+			log = log.With(
 				slog.Int64("status", int64(rw.statusCode)),
 				slog.String("request_id", string(requestID)),
+				slog.Float64("duration_ms", time.Since(start).Seconds()*1000),
 			)
+
+			log.Info("http request")
 		})
 	}
 }
