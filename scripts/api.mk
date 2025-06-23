c3cbba27e684b427310aea692499133a374b1b92
alex <donskov_a@icloud.com>
2025-06-23 12:03:43 +0500

    fix error

--- scripts/api.mk
+++ scripts/api.mk
@@ -5,7 +5,7 @@ API_CONTAINER_NAME?=docker.io/donskova1ex/${API_NAME}
 
 
 clean_api:
-	rm -rf bin/average_incomesr
+	rm -rf bin/average_incomes
 
 api_docker_build:
 	docker build --no-cache -t ${API_CONTAINER_NAME}:${API_VERSION} -t ${API_CONTAINER_NAME}:latest -f Dockerfile.api .
